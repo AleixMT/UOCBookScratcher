71d7b2a3b7a185540c8afdd38e4c02c027de67aa
Axlfc <axelfernandezcurros@gmail.com>
2019-07-27 05:37:22 +0200

    Added google translator unofficial

--- Classifier.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import os
-import sys
-import struct
-folder = os.listdir()
-for file in folder:
-	print(file)
---
+++ classifier.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import os
+import sys
+import struct
+from  googletrans import Translator
+import csv
+
+translator = Translator()
+pathPrefix = "books_organized/unknown/"
+folder = os.listdir(pathPrefix)
+
+dicts = {}
+trainingDataset = {}
+
+for file in folder:
+	file_pointer = open(pathPrefix + file)
+	line = file_pointer.readline()
+	keywords = line.replace('.', ',').split("{")[1].split("}")[0].split(",") #comentario
+	for keyword in keywords:
+		keyword = keyword.lstrip().rstrip().upper().swapcase()
+		trainingDataset[keyword] = file
+
+
+print(trainingDataset)
+langs = ["ES"]
+for lang in langs:
+	dictTemp = {}
+
+	for keyword, value in trainingDataset.items():
+		print(keyword + lang)
+		translatedKeyword = translator.translate(text=keyword, dest=lang)
+		print("Keyword " + keyword + " is translated in " + lang + " as " + translatedKeyword.text)
+		dictTemp[translatedKeyword] = value
+	dicts[lang] = dictTemp
+dicts["EN"] = trainingDataset
+langs.append("EN")
+
+for lang in langs:
+	file_pointer = open(lang + "_dictionary.csv", "w+")
+	for keyword, value in dicts[lang].items():
+		file_pointer.write(keyword + "," + value + "\n")
+	#csv.DictWriter(
